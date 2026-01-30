536de46cf8342faad9b50a63d53a889915314d68
kevinjin12 <kevinjin345@gmail.com>
2026-01-29 22:59:05 -0500

    added read_input

--- src/main.py
+++ src/main.py
@@ -1,6 +1,35 @@
-def matching_engine(n, hospital_preferences, student_preferences):
-    hospital_preferences = [[x - 1 for x in row] for row in hospital_preferences]
-    student_preferences = [[x - 1 for x in row] for row in student_preferences]
+from collections import deque
+
+def read_input(filename):
+    with open(filename, 'r') as file:
+        lines = [line.strip() for line in file]
+
+    if not lines:
+        raise ValueError("Empty input file")
+    
+    n = int(lines[0])
+
+    if len(lines) != 2 * n + 1:
+        raise ValueError(f"Expected {2*n + 1} lines, got {len(lines)}")
+    
+    hospital_prefs = []
+    for i in range(1, n + 1):
+        hospital_prefs.append([int(x) for x in lines[i].split()])
+
+    student_prefs = []
+    for i in range(n + 1, 2 * n + 1):
+        student_prefs.append([int(x) for x in lines[i].split()])
+
+    if len(hospital_prefs) != n or len(student_prefs) != n:
+        raise ValueError("Number of hospitals and students must be equal")
+
+    return n, hospital_prefs, student_prefs
+
+
+def matching_engine(n, hospital_prefs, student_prefs):
+    # change perference list input from 1-indexed to 0-indexed
+    hospital_prefs = [[x - 1 for x in row] for row in hospital_prefs]
+    student_prefs = [[x - 1 for x in row] for row in student_prefs]
 
     # pairs from the perspective of the hospitals
     hospital_pairs = [-1] * n
@@ -8,48 +37,58 @@ def matching_engine(n, hospital_preferences, student_preferences):
     # pairs from the perspective of the students
     student_pairs = [-1] * n
 
-    next_proposal = [0] * n
+    # keeps track of next student each hospital wants to match with
+    next_student = [0] * n
 
-    student_rank = [[0]*n for _ in range(n)]
+    # create 2d matrix for easy lookup of rankings
+    student_ranks = [[0] * n for i in range(n)]
     for student in range(n):
-        for rank, hospital in enumerate(student_preferences[student]):
-            student_rank[student][hospital] = rank
+        for rank, hospital in enumerate(student_prefs[student]):
+            student_ranks[student][hospital] = rank
 
-    while True:
-        progress = False
+    unmatched_hospitals = deque(range(n))
 
-        for hospital in range(n):
-            if hospital_pairs[hospital] == -1 and next_proposal[hospital] < n:
-                student = hospital_preferences[hospital][next_proposal[hospital]]
-                next_proposal[hospital] += 1
-                progress = True
+    while unmatched_hospitals:
+        h = unmatched_hospitals.popleft()
 
-                if student_pairs[student] == -1:
-                    hospital_pairs[hospital] = student
-                    student_pairs[student] = hospital
-                else:
-                    current_hospital = student_pairs[student]
-                    if student_rank[student][hospital] < student_rank[student][current_hospital]:
-                        student_pairs[student] = hospital
-                        hospital_pairs[current_hospital] = -1
-                        hospital_pairs[hospital] = student
+        # top student on h's list whom h has not been matched
+        s = hospital_prefs[h][next_student[h]]
 
-        if not progress:
-            break
-    
+        # student is free
+        if student_pairs[s] == -1:
+            hospital_pairs[h] = s
+            student_pairs[s] = h    
+
+        # student prefers hospital h to current assignment
+        elif student_ranks[s][h] < student_ranks[s][student_pairs[s]]:
+            old_h = student_pairs[s]
+            hospital_pairs[h] = s
+            student_pairs[s] = h
+            # unmatch old hospital
+            hospital_pairs[old_h] = -1
+            unmatched_hospitals.append(old_h)
+
+        # student rejects hospital h
+        else:
+            unmatched_hospitals.append(h)
+
+        # hospital h did not match with student s so move on to next preference
+        next_student[h] += 1
+
     res = []
     for hospital, student in enumerate(hospital_pairs):
-        res.append((hospital + 1, student + 1))
+        res.append([hospital + 1, student + 1])
+
     return res
 
 def main():
-    hospital_preferences = [[1, 2, 3], [2, 3, 1], [2, 1, 3]]
-    student_preferences = [[2, 1, 3], [1, 2, 3], [1, 2, 3]]
-    n = len(hospital_preferences)
+    n, hospital_prefs, student_prefs = read_input("tests/test1.in")
 
-    hospital_pairs = matching_engine(n, hospital_preferences, student_preferences)
+    hospital_pairs = matching_engine(n, hospital_prefs, student_prefs)
 
-    print("Hospital pairs:", hospital_pairs)
+    print(hospital_pairs)
+
+
 
 if __name__ == "__main__":
     main()
